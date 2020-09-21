a89c1c75e111ea494f1156fb926666814e36c642
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2020-09-21 20:38:48 +0200

    remove unused trie implementation

--- src/Wordlist.elm
+++ src/Wordlist.elm
@@ -3,33 +3,6 @@ module Wordlist exposing (Wordlist, fromList, lookup, anagramsOf, fromFrequencyL
 import Set exposing (Set)
 import Dict exposing (Dict)
 
-type Trie = Trie (Dict Char Trie)
-
-trieLookup : List Char -> Trie -> Bool
-trieLookup l (Trie d) = case l of
-  (x::xs) -> case Dict.get x d of
-    Nothing -> False
-    Just st -> trieLookup xs st
-  [] -> True
-
-convertWord : String -> List Char
-convertWord w = (String.toList w) ++ [terminator]
-
-trieAdd : Trie -> List Char -> Trie
-trieAdd (Trie d) l = case l of
-  [] -> Trie d
-  (x::xs) -> let update mt = Just (case mt of
-                                     Just t -> trieAdd t xs
-                                     Nothing -> trieAdd emptyTrie xs)
-             in Trie (Dict.update x update d)
-
-trieAddWord : String -> Trie -> Trie
-trieAddWord s t = trieAdd t (convertWord s)
-
-emptyTrie = Trie Dict.empty
-
-terminator = '.'
-
 type Wordlist = Wordlist (Set String) (Dict String (List String))
 
 fromList : List String -> Wordlist
